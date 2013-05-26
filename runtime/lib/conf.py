df8dad0b08090787ad9ea90d5102d13e1d5d6317
Sergey Panfilov <sergray@gmail.com>
2013-05-26 16:26:11 +0400

    Cleanup conf module, reduce pylint errors

--- runtime/lib/conf.py
+++ runtime/lib/conf.py
@@ -1,19 +1,24 @@
-from ConfigParser import ConfigParser
+from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 import os
 
 cfg = None
 
 def __load():
+    """Load application config from INI file and store in global variable"""
     global cfg
-    dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(dir, '..', 'etc', 'app.ini')
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(this_dir, '..', 'etc', 'app.ini')
     cfg = ConfigParser()
     cfg.read(path)
 
 def get(section, option, val=''):
-    r = cfg.get(section, option)
-    if r != None:
-	return r
-    return val
+    """Lookup value of option in section. If section or option is missing,
+    then default val is returned."""
+    try:
+        retval = cfg.get(section, option)
+    except (NoSectionError, NoOptionError):
+        return val
+    else:
+        return retval
 
 __load()
